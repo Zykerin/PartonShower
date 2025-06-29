c7c4e1a11a1faaaa85fc7424d79785cdac999668
Zykerin <CGPhysics@pm.me>
2025-06-29 17:49:28 -0400

    Missed alphaS, splitting functions, and constants files in previous commit

--- juliaPartonShower/Constants.jl
+++ juliaPartonShower/Constants.jl
@@ -1,23 +1,17 @@
 include("alphaS.jl")
 
 
-# The QCD constants
-#global const Nc = 3 # The number of colors
-#global const Cf = (Nc^2 -1)/ (2 * Nc) # The quark color factor
-#global const Ca = Nc
-#global const Tr = 1/2
 
 
 
-global const t0 = 1
-global const Q = 1000
+
+#global const t0 = 1
+#global const Q = 1000
 global const Qc = 0.935
+#global const Qc::Float64 = 0.65471
 aSover::Float64 = AlphaQ(Qc) # The overestimate for the coupling constants
 
-#global const nbins: = 30
-#global const Nevolve = 10000
-
-global const pTmin = 0.65471
+global const pTmin::Float64 = 0.65471
 global const pT2min::Float64 = pTmin^2
 
 
--- juliaPartonShower/alphaS.jl
+++ juliaPartonShower/alphaS.jl
@@ -1,25 +1,11 @@
 global const Nc = 3 # The number of colors
 global const Cf = (Nc^2 -1)/ (2 * Nc) # The quark color factor
-global const Ca = Nc
+global const Ca= Nc
 global const Tr = 1/2
 
-#=
-Base.@kwdef mutable struct alphaS
-    asmz::Float64
-    mz::Float64
-    mb::Float64 = 4.75
-    mc::Float64 = 1.27
-    order::Int = 1
-    mz2::Float64 = mz^2>
-    mc2::Float64 = mc^2
-    mb2::Float64 = mb^2
-    #alphaS() = new()
-    #asmb::Float64 = AlphaQ(alpha, mb)
-    #asmc::Float64 = AlphaQ(self, mc)
-end
-=#
 asmz = 0.118
 mz = 91.1876
+order = 1
 mb = 4.75
 mc = 1.27
 order = 1
@@ -27,11 +13,45 @@ mz2 = mz^2
 mc2 = mc^2
 mb2 = mb^2
 
+#=
+function GetAlphaS(t::Float64, z::Float64)
+    mb = 4.75
+    mc = 1.27
+    order = 1
+    mz2 = mz^2
+    mc2 = mc^2
+    mb2 = mb^2
+    asmb = AlphaQ(mb)
+    asmc = AlphaQ(mc)
+    scale = z * (1 -z) * sqrt(t)
+    if scale < Qc
+        scale = Qc
+    end
+    Q = scale^2
+    if Q>= mb2
+        tref = mz2
+        asref = asmz
+        b0 = Beta0(5) / (2 * pi)
+        b1 = Beta1(5)/ (2 * pi)^2
+    elseif Q >= mc2
+        tref = mb2
+        asref = asmb
+        b0 = Beta0(4) / (2 * pi)
+        b1 = Beta1(4)/ (2 * pi)^2
+    else
+        tref = mc2
+        asref = asmc
+        b0 = Beta0(3) / (2 * pi)
+        b1 = Beta1(3)/ (2 * pi)^2
+    end
+    w = 1 + b0 * asref * log(t/tref)
+    return asref / w * (1 - b1 / b0 * asref * log(w) / w)
 
-#struct alphaS
-#    asmz::Float64
-#    mz::Float64
-#end
+    
+
+end
+=#
+
 
 
 function As1(t::Float64)
@@ -72,17 +92,16 @@ function AlphaQ(Q::Float64)
    end
 end
 
-
-function GetAlphaS(t, z)
-   scale = z * (1 -z) * sqrt(t)
-   if scale < Qc
-       return AlphaQ(Qc)
-   else
-       return AlphaQ(scale)
-   end
-end
-
 asmb = AlphaQ(mb)
 asmc = AlphaQ(mc)
 
+
+function getAlphaS(t::Float64, z::Float64, Qcut::Float64)
+    scale = z * (1- z) * sqrt(t)
+    if scale < Qcut
+        scale = Qcut
+    end
+    return AlphaQ(scale)
+    
+end
 
